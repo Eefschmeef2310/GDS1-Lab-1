94bd4c7f011525ed43a1cb4ecfad47c3b57e0b3f
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-02-25 10:48:41 +1100

    credits scene added

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const CREDITS = preload("res://menus/credits.tscn")
+
 func _ready():
 	for i in get_tree().get_nodes_in_group("bullet"):
 		i.queue_free()
@@ -10,3 +12,6 @@ func _on_quit_pressed():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Levels/Main.tscn")
+
+func _on_credits_pressed():
+	add_child(CREDITS.instantiate())
