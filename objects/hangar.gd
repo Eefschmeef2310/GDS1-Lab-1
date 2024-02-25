9a568491fa8949b5621f9b757a1e46f9134ed560
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-02-25 17:16:30 +1100

    everything complete!

--- objects/hangar.gd
+++ objects/hangar.gd
@@ -12,3 +12,4 @@ func _on_body_entered(body):
 	if body.is_in_group("player") && body.soldiers_carrying > 0:
 		soldier_dropped.emit()
 		body.soldiers_carrying = 0
+		$GPUParticles2D.emitting = true
